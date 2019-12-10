1cc5933980d2ac25a8bc6d44ac15c819918b29cc
tanjeeb <tanjeeb21@gmail.com>
2019-12-10 10:24:14 +0600

    Front-end all here old code

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+const status = (process.env.NODE_ENV === 'production') ? 'error' : 'warn';
+
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:nuxt/recommended',
+    'plugin:vue/strongly-recommended',
+    'prettier',
+    '@vue/airbnb',
+  ],
+  plugins: ['prettier'],
+  rules: {
+    'nuxt/no-cjs-in-config': 'off',
+    'indent': [
+      status,
+      2
+    ],
+    'quotes': [
+      status,
+      'single'
+    ],
+    'semi': [
+      status,
+      'always'
+    ],
+    'import/extensions': ['error', 'always', {
+      'js': 'never',
+      'vue': 'never'
+    }],
+    "camelcase": [
+      0, {
+        "properties": "never"
+      }
+    ],
+    "no-underscore-dangle": [
+      0, {
+        "properties": "never"
+      }
+    ],
+    'no-console': status,
+    'no-debugger': status,
+    'max-len': 'off',
+    'linebreak-style': ["error", "unix"],
+    'import/prefer-default-export': 'off',
+    'vue/html-closing-bracket-newline': [status, {
+      'singleline': 'never',
+      'multiline': 'never'
+    }]
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+};
---
+++ build.sh
@@ -0,0 +1,5 @@
+#export NODE_ENV=production
+# export APP_URL=http://localhost:3000
+#export API_URL=https://api-dot-paperdaz-dev.appspot.com
+yarn
+yarn build
